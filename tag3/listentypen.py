4aa927dc330b2803e650e0d20785a54cbc2fb872
Ulrich Cuber <ulrich@wtfoxtrot.de>
2025-05-22 12:29:29 +0200

    tag 3

---
+++ tag3/listentypen.py
@@ -0,0 +1,15 @@
+list1 = ['This is', 'the only tool']
+list2 = ['that', 'lets', 'you', 'visually']
+text = ' '.join(list1 + list2)
+print(text)
+ 
+myTuple = ('debug code', 'step-by-step!')
+text = ' '.join(myTuple)
+print(text)
+ 
+fruitSet = {'apple', 'apple',
+            'banana',
+            'cherry',
+            'durian'}
+text = ' '.join(fruitSet)
+print(text)

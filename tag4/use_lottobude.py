e39ba94622752b9f3e7a3d139752f7036f67f391
Ulrich Cuber <ulrich@wtfoxtrot.de>
2025-10-28 12:43:36 +0100

    Aufgeräumt

--- tag4/use_lottobude.py
+++
@@ -1,8 +0,0 @@
-import lottobude as lb # lottobude ist mir zu lang, ich nehme einen alias "lb"
-
-anzahl = int(input("Wie oft wollen Sie spielen? "))
-
-for _ in range(anzahl):
-    lb.lotto_bude()
-
-print("Das war's für heute. Viel Glück beim nächsten Mal!")
